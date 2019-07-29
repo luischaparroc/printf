8dced5458e53b9b410d2a750182970fd82b21854
diegolopezq95 <927@holbertonschool.com>
2019-07-29 22:32:30 +0000

    [Feature #17]: Function that prints the value of non-printed chars Create print_usr.c Modified get_print_func.c and holberton.h

--- holberton.h
+++ holberton.h
@@ -28,6 +28,7 @@ int print_unt(va_list arguments, char *buf, unsigned int ibuf);
 int print_oct(va_list arguments, char *buf, unsigned int ibuf);
 int print_hex(va_list arguments, char *buf, unsigned int ibuf);
 int print_upx(va_list arguments, char *buf, unsigned int ibuf);
+int print_usr(va_list arguments, char *buf, unsigned int ibuf);
 int (*get_print_func(char s))(va_list, char *, unsigned int);
 unsigned int handl_buf(char *buf, char c, unsigned int ibuf);
 int print_buf(char *buf, unsigned int nbuf);
---
+++ print_usr.c
@@ -0,0 +1,37 @@
+#include "holberton.h"
+/**
+ * print_usr - writes the character c to stdout
+ * @arguments: input string
+ * @buf: buffer pointer
+ * @ibuf: index for buffer pointer
+ * Return: number of chars printed
+ */
+int print_usr(va_list arguments, char *buf, unsigned int ibuf)
+{
+	unsigned char *str;
+	char *hexadecimal, *binary;
+	unsigned int i, sum, op;
+
+	str = va_arg(arguments, unsigned char *);
+	binary = malloc(sizeof(char) * (32 + 1));
+	hexadecimal = malloc(sizeof(char) * (8 + 1));
+	for (sum = i = 0; str[i]; i++)
+	{
+		if (str[i] < 32 || str[i] >= 127)
+		{
+			ibuf = handl_buf(buf, '\\', ibuf);
+			ibuf = handl_buf(buf, 'x', ibuf);
+			op = str[i];
+			binary = fill_binary_array(binary, op, 0, 32);
+			hexadecimal = fill_hex_array(binary, hexadecimal, 1, 8);
+			ibuf = handl_buf(buf, hexadecimal[6], ibuf);
+			ibuf = handl_buf(buf, hexadecimal[7], ibuf);
+			sum += 3;
+		}
+		else
+			ibuf = handl_buf(buf, str[i], ibuf);
+	}
+	free(binary);
+	free(hexadecimal);
+	return (i + sum);
+}
