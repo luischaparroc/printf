a5709b771c0c98c3f09094bb7dcbfc786f45a6e8
diegolopezq95 <927@holbertonschool.com>
2019-07-30 00:06:21 +0000

    [Feature #23]: Prints the string in reverse Create print_rev.c Modified get_print_func.c and holberton.h

---
+++ print_rev.c
@@ -0,0 +1,32 @@
+#include "holberton.h"
+
+/**
+ * print_rev - writes the str in reverse
+ * @arguments: input string
+ * @buf: buffer pointer
+ * @ibuf: index for buffer pointer
+ * Return: number of chars printed.
+ */
+int print_rev(va_list arguments, char *buf, unsigned int ibuf)
+{
+	char *str;
+	unsigned int i;
+	int j = 0;
+	char nill[] = "(llun)";
+
+	str = va_arg(arguments, char *);
+	if (str == NULL)
+	{
+		for (i = 0; nill[i]; i++)
+			ibuf = handl_buf(buf, nill[i], ibuf);
+		return (6);
+	}
+	for (i = 0; str[i]; i++)
+		;
+	j = i - 1;
+	for (; j >= 0; j--)
+	{
+		ibuf = handl_buf(buf, str[j], ibuf);
+	}
+	return (i);
+}
